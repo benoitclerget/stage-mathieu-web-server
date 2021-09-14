4120b3a64293fdeb8a84d9ca702de977b73be622
benoitclerget <benoit.clerget@fr.ibm.com>
2021-09-14 11:16:09 +0200

    add api/app_news API

--- server/routers/apis.js
+++ server/routers/apis.js
@@ -27,6 +27,9 @@ module.exports = function(app, appName, appVersion, gitRepo) {
   router.get('/api/app_news', async function(req, res) {
     let result = {
       err: null,
+      titre: `Welcome to SCMPL slack bot application`,
+      text1: `One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. The bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked. "What's happened to me?" he thought. It wasn't a dream. His room, a proper human`,
+      text2: `Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to`,
       news: [
         {
           title: 'This is the title of the latest news',
