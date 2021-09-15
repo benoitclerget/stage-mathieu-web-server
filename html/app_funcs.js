89bbb399f0673357aec8439a7c5a09a2a48fa1a9
mathrod1993 <rodriguezbalaruc@gmail.com>
2021-09-15 16:57:37 +0200

    ajout de cartes dynamique

--- html/app_funcs.js
+++ html/app_funcs.js
@@ -3,6 +3,7 @@
 document.addEventListener("DOMContentLoaded", async function() {
   if (window.fetch) {
 	  otherapi()
+	  createcards()
     try{
       let response = await fetch('/api/app_news')
       if(response.ok) {
@@ -51,6 +52,42 @@ try{
     }
 }
 
+async function createcards(){
+	try{
+let response = await fetch('/api/app_news')
+if(response.ok) {
+        response.json().then(function(myJson) {
+          if (!myJson.err) {
+            // set values into the dom
+			console.log(myJson)
+			var cardlist = myJson.cards
+			console.log(cardlist)
+			var cardsHTML = "";
+			var currentlink = "";
+			//loop
+			for (let i = 0; i < cardlist.length; i++) {
+				if (cardlist[i].link){currentlink = "<button class='bx--btn bx--btn--secondary'><a href=" + cardlist[i].link.url + " target='_blank'>" + cardlist[i].link.text + "</a></button>"}   
+				cardsHTML += "<div class='bx--col bx--col-md-4 bx--col-sm-4'><div class='bx--tile'><div class='bx--tile__img'><img src='" + cardlist[i].img.src + "' /></div><div class='bx--tile__content'><h2>" + cardlist[i].title + "</h2><p>" + cardlist[i].content + "</p>" + currentlink + "</div></div></div>";
+				/* cardlist[i].title + cardlist[i].content + cardlist[i].link + "<br>" */		
+				currentlink = "";
+			}
+			
+			var h = document.getElementById("secondline");
+h.insertAdjacentHTML("afterend", cardsHTML); 
+			
+			console.log(cardsHTML);
+          } else {
+            console.error('API error: ' + myJson.message);
+          }
+        });
+      } else {
+        console.error('response error: ' + response.ok);
+      }
+}
+ catch(error) {
+      console.error('Error with fetch operation: ' + error);
+    }
+}
 
 function formatMemNb(bytes) {
 	let sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB']
